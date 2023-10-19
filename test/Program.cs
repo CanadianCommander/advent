e8bd9743702c89067b7af46874bd0b674d46d0bd
Benjamin Benetti <ben@bbenetti.ca>
2023-10-18 18:53:08 -0700

    init commit

---
+++ test/Program.cs
@@ -0,0 +1,5 @@
+﻿
+Console.WriteLine("Hello World!");
+
+var second = new Second();
+second.DoTheShiz();
---
+++ test/Second.cs
@@ -0,0 +1,9 @@
+
+
+public class Second
+{
+  public void DoTheShiz()
+  {
+    Console.WriteLine("Second?");
+  }
+}
