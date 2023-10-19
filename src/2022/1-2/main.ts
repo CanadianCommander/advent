0209a3c708616b9caa8fa74b7a1fff51c92d0f68
Benjamin Benetti <ben@bbenetti.ca>
2023-10-18 20:26:51 -0700

    Day 1-2

---
+++ src/2022/1-2/main.ts
@@ -0,0 +1,18 @@
+import * as fs from "fs";
+
+const fileRaw = fs.readFileSync("./src/2022/1/input.txt", "utf-8");
+
+let calElf = fileRaw.trim().split("\n\n");
+
+const top3 = calElf
+  .map((calStr) =>
+    calStr
+      .split("\n")
+      .map((numStr) => parseInt(numStr, 10))
+      .reduce((total, foodCal) => total + foodCal, 0)
+  )
+  .sort((elfCal0, elfCal1) => elfCal1 - elfCal0)
+  .slice(0, 3)
+  .reduce((acc, elfCal) => acc + elfCal, 0);
+
+console.log("The Answer is!", top3);
